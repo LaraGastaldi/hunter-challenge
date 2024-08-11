a8001cca06803adc208683e72daefad3e8b2b134
lara <lara@gototem.com.br>
2024-08-11 13:18:03 -0300

    all routes are api routes, added handler to app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Exceptions\Handler as ExceptionsHandler;
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Exceptions\Handler;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        // web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        api: __DIR__.'/../routes/web.php',
+        apiPrefix: '',
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->withSingletons([
+        Handler::class => ExceptionsHandler::class,
+    ])->create();
